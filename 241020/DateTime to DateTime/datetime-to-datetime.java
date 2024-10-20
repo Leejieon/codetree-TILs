f98a500872cec9b86b54c6bb6ebedbbc36fddd48
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-10-20 18:42:48 +0900

    Title: DateTime to DateTime, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 241020/DateTime to DateTime/datetime-to-datetime.java
+++ 241020/DateTime to DateTime/datetime-to-datetime.java
@@ -11,6 +11,6 @@ public class Main {
         int standard = 11 * 24 * 60 + 11 * 60 + 11;
         int cur = a * 24 * 60 + b * 60 + c;
 
-        System.out.println(cur - standard);
+        System.out.println(cur - standard < 0 ? -1 : cur - standard);
     }
 }
