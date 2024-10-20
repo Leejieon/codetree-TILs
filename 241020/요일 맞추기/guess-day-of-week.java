2a0dd82181d5edaed0aaf28a4e1600075071eb51
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-10-20 20:11:35 +0900

    Title: 요일 맞추기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241020/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    static String[] daysStr = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    static String[] daysStrReverse = {"Mon", "Sun", "Sat", "Fri", "Thu", "Web", "Tue"};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        int standard = getDay(m1, d1);
+        int cur = getDay(m2, d2);
+
+        if(standard < cur) {
+            System.out.println(daysStr[(cur - standard)%7]);
+        } else {
+            System.out.println(daysStrReverse[(standard - cur)%7]);
+        }
+    }
+
+    static int getDay(int m, int d) {
+        int day = 0;
+        for(int i = 0; i < m; i++) {
+            day += days[i];
+        }
+        return day + d;
+    }
+}
