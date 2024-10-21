4245840d33dd1d5c4c57d3ddbeb31dbbe26c3565
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-10-21 22:50:02 +0900

    Title: 아름다운 수, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241021/아름다운 수/beautiful-number.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+
+    static int n, answer;
+    static int[] order;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        order = new int[n];
+        answer = 0;
+
+        permutation(0);
+        System.out.println(answer);
+    }
+
+    static void permutation(int depth) {
+        // Base Case
+        if(depth == n) {
+            if(success()) answer++;
+            return;
+        }
+
+        // Recursive Case
+        for(int cand = 1; cand <= 4; cand++) {
+            order[depth] = cand;
+            permutation(depth + 1);
+        }
+    }
+
+    static boolean success() {
+        boolean result = false;
+        int count = 1;
+        int prev = order[0];
+        for(int i = 1; i < n; i++) {
+            if(order[i] == prev) {
+                count++;
+            } else {
+                if(count % prev == 0) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            }
+            prev = order[i];
+        }
+
+        if(count % prev == 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+        
+        return result;
+    }
+}
