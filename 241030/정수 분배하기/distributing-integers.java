699a18992c3cf9854822389eb734bf9fd13f4ef0
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-10-30 13:57:20 +0900

    Title: 정수 분배하기, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241030/정수 분배하기/distributing-integers.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n, m, max;
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        max = Integer.MIN_VALUE;
+
+        numbers = new int[n];
+        for(int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, numbers[i]);
+        }
+
+        int left = 1;
+        int right = max;
+        int idx = -1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(getCount(mid) >= m) {
+                left = mid + 1;
+                idx = Math.max(idx, mid);
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(idx);
+    }
+
+    static int getCount(int standard) {
+        int result = 0;
+        for(int number : numbers) {
+            result += number / standard;
+        }
+        return result;
+    }
+}
