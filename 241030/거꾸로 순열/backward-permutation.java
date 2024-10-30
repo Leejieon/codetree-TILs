16ade4e5cf58da2da26d45fe70fd09843c81890e
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-10-30 21:33:10 +0900

    Title: 거꾸로 순열, Time: 1181ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 241030/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    
+    static int n;
+    static boolean[] visited;
+    static int[] order;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        visited = new boolean[n + 1];
+        order = new int[n + 1];
+
+        permutation(1);
+    }
+
+    static void permutation(int depth) {
+        // Base Case
+        if(depth == n + 1) {
+            for(int i = 1; i <= n; i++) {
+                System.out.print(order[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        // Recursive Case
+        for(int i = n; i > 0; i--) {
+            if(visited[i]) continue;
+
+            visited[i] = true;
+            order[depth] = i;
+            permutation(depth + 1);
+            visited[i] = false;
+        }
+    }
+
+}
