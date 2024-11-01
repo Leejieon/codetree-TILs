3d8cc78f4762186e04318a1ed626e8d07b5be49b
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-11-01 23:30:48 +0900

    Title: 삼 오 무, Time: 126ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241101/삼 오 무/three-five-moo.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int left = 1;
+        int right = 1_000_000_000;
+        int rst = 0;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(count(mid) == n) {
+                rst = mid;
+                break;
+            }
+
+            if(count(mid) > n) {
+                right = mid  -1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(rst);
+    }
+
+    static int count(int number) {
+        return number - (number/3 + number/5 - number/15);
+    }
+}
