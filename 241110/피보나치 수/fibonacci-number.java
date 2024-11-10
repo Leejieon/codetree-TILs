a25d1422066938b7c17d0fee7a432a7cda62dee0
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-11-10 15:34:42 +0900

    Title: 피보나치 수, Time: 123ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241110/피보나치 수/fibonacci-number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        System.out.println(fibbo(n));
+    }
+
+    static int fibbo(int number) {
+        // Base Case
+        if(number <= 2) {
+            return dp[number];
+        }
+
+        if(dp[number] != -1) {
+            return dp[number];
+        }
+
+        // Recursive Case
+        dp[number] = fibbo(number - 1) + fibbo(number - 2);
+        return dp[number];
+    }
+}
