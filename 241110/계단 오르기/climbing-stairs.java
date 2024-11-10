8621b69805ca812dd6e258a4eff80afd63efe1ce
leejieon <42794501+Leejieon@users.noreply.github.com>
2024-11-10 16:00:10 +0900

    Title: 계단 오르기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 241110/계단 오르기/climbing-stairs.java
+++ 241110/계단 오르기/climbing-stairs.java
@@ -5,14 +5,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        long[] dp = new long[N + 2];
+        int[] dp = new int[N + 2];
         dp[2] = 1;
         dp[3] = 1;
 
         for(int i = 4; i <= N; i++) {
-            dp[i] = dp[i - 2] + dp[i - 3];
+            dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
         }
 
-        System.out.println(dp[N] % 10007);
+        System.out.println(dp[N]);
     }
 }
