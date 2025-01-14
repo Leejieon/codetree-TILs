1b9c7a035c97b5dc67a2fb975ed0017c71440084
leejieon <42794501+Leejieon@users.noreply.github.com>
2025-01-14 17:42:08 +0900

    Title: 최대 동전 거슬러주기, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250114/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[M + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for(int i = 1; i <= M; i++) {
+            for(int j = 0; j < N; j++) {
+                if(i >= coins[j]) {
+                    if(dp[i - coins[j]] == -1) continue;
+                    dp[i] = Math.max(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[M]);
+    }
+}
