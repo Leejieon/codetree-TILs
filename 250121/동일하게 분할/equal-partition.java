f251f608d71c9974f7ff48f8b38d9ba9e584142b
leejieon <42794501+Leejieon@users.noreply.github.com>
2025-01-21 11:40:27 +0900

    Title: 동일하게 분할, Time: 111ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250121/동일하게 분할/equal-partition.java
+++ 250121/동일하게 분할/equal-partition.java
@@ -16,8 +16,13 @@ public class Main {
             total += numbers[i];
         }
 
+        if(total % 2 == 1) {
+            System.out.println("No");
+            return;
+        }
+
         boolean[][] dp = new boolean[N + 1][total + 1];
-        dp[0][0] = true;
+
         for(int i = 1; i <= N; i++) {
             dp[i][0] = true;
             for(int s = 1; s <= total; s++) {
@@ -29,6 +34,6 @@ public class Main {
             }
         }
 
-        System.out.println(dp[N][total/2] ? "Yes" : "No");
+        System.out.println(dp[N][total/2 + 1] ? "Yes" : "No");
     }
 }
