41d7426e9673d6e390d82d4a2e8e1f82eed646a3
leejieon <42794501+Leejieon@users.noreply.github.com>
2025-01-24 18:52:14 +0900

    Title: 최소 점프 횟수, Time: 79ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250124/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] jumps = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            jumps[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, 101);
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = N; j > i; j--) {
+                if(j - i <= jumps[i]) {
+                    if(dp[i] == 101) continue;
+                    dp[j] = Math.min(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[N] == 101 ? -1 : dp[N]);
+    }
+}
