4edb4861c5b2a32d4d93091efad8100e2d99984f
leejieon <42794501+Leejieon@users.noreply.github.com>
2025-02-06 20:29:22 +0900

    Title: 연결된 정점 2, Time: 3176ms, Memory: 96MB, Status: Time Limit Exceed - CodeTree

--- 250206/연결된 정점 2/connected-vertex-2.java
+++ 250206/연결된 정점 2/connected-vertex-2.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
 
     static int N;
-    static int[] parents;
+    static HashMap<Integer, Integer> parents = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,23 +12,24 @@ public class Main {
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-
-        parents = new int[100_001];
-        for(int i = 1; i <= 100_000; i++) {
-            parents[i] = i;
-        }
-
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
+            if(!parents.containsKey(a)) {
+                parents.put(a, a);
+            }
+            if(!parents.containsKey(b)) {
+                parents.put(b, b);
+            }
+
             union(a, b);
             
             int root = find(b);
             int count = 0;
-            for(int j = 1; j <= 100_000; j++) {
-                if(parents[j] == root) count++;
+            for(int node : parents.keySet()) {
+                if(parents.get(node) == root) count++;
             }
             sb.append(count).append("\n");
         }
@@ -38,11 +39,12 @@ public class Main {
     static void union(int x, int y) {
         x = find(x);
         y = find(y);
-        parents[y] = x;
+        parents.put(y, x);
     }
 
     static int find(int x) {
-        if(parents[x] == x) return x;
-        return parents[x] = find(parents[x]);
+        if(parents.get(x) == x) return x;
+        parents.put(x, find(parents.get(x)));
+        return parents.get(x);
     }
 }
