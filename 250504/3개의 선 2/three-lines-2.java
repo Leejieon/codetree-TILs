809783be4dc5a9bd6b53b37298d4811771c7fca5
leejieon <42794501+Leejieon@users.noreply.github.com>
2025-05-04 15:56:54 +0900

    Title: 3개의 선 2, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

--- 250504/3개의 선 2/three-lines-2.java
+++ 250504/3개의 선 2/three-lines-2.java
@@ -5,10 +5,9 @@ public class Main {
 
     static int N;
     static boolean isPossible;
-    static int[] axis;
     static HashMap<Integer, ArrayList<Integer>> xmap = new HashMap<>();
     static HashMap<Integer, ArrayList<Integer>> ymap = new HashMap<>();
-    static boolean[][] checked = new boolean[11][11];
+    static int[][] checked = new int[11][11];
     static ArrayList<int[]> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
@@ -34,20 +33,17 @@ public class Main {
             list.add(new int[]{x, y});
         }
 
-        proceed(0, 0, 0);
+        proceed(0, 0);
         System.out.println(isPossible ? 1 : 0);
     }
 
-    static void proceed(int cur, int depth, int count) {
+    static void proceed(int cur, int count) {
         // Base Case
         if(isPossible) {
             return;
         }
-        if(depth >= 3) {
-            // if(check()) {
-            //     isPossible = true;
-            // }
-            if(count >= N) {
+        if(count >= 3) {
+            if(check()) {
                 isPossible = true;
             }
             return;
@@ -57,25 +53,23 @@ public class Main {
         for(int i = cur; i < 22; i++) {
             if(i < 11) {
                 if(xmap.containsKey(i)) {
-                    // for(int y : xmap.get(i)) {
-                    //     checked[i][y] = true;
-                    // }
-                    // proceed(i + 1, count + 1);
-                    // for(int y : xmap.get(i)) {
-                    //     checked[i][y] = false;
-                    // }
-                    proceed(i + 1, depth + 1, count + xmap.get(i).size());
+                    for(int y : xmap.get(i)) {
+                        checked[i][y]++;
+                    }
+                    proceed(i + 1, count + 1);
+                    for(int y : xmap.get(i)) {
+                        checked[i][y]--;
+                    }
                 }
             } else {
                 if(ymap.containsKey(i - 11)) {
-                    // for(int x : ymap.get(i - 11)) {
-                    //     checked[x][i - 11] = true;
-                    // }
-                    // proceed(i + 1, count + 1);
-                    // for(int x : ymap.get(i - 11)) {
-                    //     checked[x][i - 11] = false;
-                    // }
-                    proceed(i + 1, depth + 1, count + ymap.get(i - 11).size());
+                    for(int x : ymap.get(i - 11)) {
+                        checked[x][i - 11]++;
+                    }
+                    proceed(i + 1, count + 1);
+                    for(int x : ymap.get(i - 11)) {
+                        checked[x][i - 11]--;
+                    }
                 }
             }
         }
@@ -83,7 +77,7 @@ public class Main {
 
     static boolean check() {
         for(int[] point : list) {
-            if(!checked[point[0]][point[1]]) {
+            if(checked[point[0]][point[1]] == 0) {
                 return false;
             }
         }
