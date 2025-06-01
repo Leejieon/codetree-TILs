3e9788e4c42a55f8f73ecd9daa3dbb8c08a71829
leejieon <42794501+Leejieon@users.noreply.github.com>
2025-06-01 22:49:17 +0900

    Title: 금 채굴하기, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250601/금 채굴하기/gold-mining.java
+++ 250601/금 채굴하기/gold-mining.java
@@ -1,58 +1,67 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_NUM = 20;
+    
+    public static int n, m;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    
+    public static int getArea(int k) {
+        return k * k + (k+1) * (k+1); 
+    }
+    
+    public static boolean inRange(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }
+    
+    public static int getNumOfGoldInBorder(int row, int col, int k) {
+        int numOfGold = 0;
+        int[] dx = new int[]{1, 1, -1, -1};
+        int[] dy = new int[]{-1, 1, 1, -1}; // 방향에 따라 바뀌는 x와 y의 변화량을 정의합니다.
+    
+        if(k == 0)
+            return grid[row][col];
+    
+        int currX = row - k, currY = col; // 순회 시작점 설정
+    
+        for(int currDir = 0; currDir < 4; currDir++) {
+            for(int step = 0; step < k; step++) {
+                if(inRange(currX, currY)) {
+                    numOfGold += grid[currX][currY];
+                }
+                currX = currX + dx[currDir];
+                currY = currY + dy[currDir];
+            }
+        }
+    
+        return numOfGold;
+    }
 
-    static int N, M;
-    static int[][] graph;
-    static HashSet<Point> goldSet = new HashSet<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxGold = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int row = 0; row < n; row++)
+            for(int col = 0; col < n; col++)
+                grid[row][col] = sc.nextInt();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        for(int y = 0; y < N; y++) {
-            st = new StringTokenizer(br.readLine());
-            for(int x = 0; x < N; x++) {
-                graph[y][x] = Integer.parseInt(st.nextToken());
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++) {
+                int numOfGold = 0;
+                for(int k = 0; k <= 2 * (n-1); k++) {
+                    // 이전 k까지 구한 금의 개수에
+                    // 해당 k의 모서리에 존재하는 금의 개수를 더해줍니다.
+                    numOfGold += getNumOfGoldInBorder(row, col, k);
 
-                if(graph[y][x] == 1) {
-                    goldSet.add(new Point(y, x));
+                    if(numOfGold * m >= getArea(k))
+                        maxGold = Math.max(maxGold, numOfGold);
                 }
             }
         }
 
-        for(int y = 0; y < N; y++) {
-            for(int x = 0; x < N; x++) {
-                
-            }
-        }
-    }
-
-
-
-    static class Point {
-        int y, x;
-
-        Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Point other = (Point) obj;
-            return this.y == other.y && this.x == other.x;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(y, x);
-        }
+        System.out.print(maxGold);
     }
 }
